02623a1a06125a67a0e3a121661cc621a13ef463
notpr1me <minimofreeman@gmail.com>
2023-09-21 09:19:26 -0700

    reset all animations

--- Assets/Characters/Player/PlayerController.cs
+++ Assets/Characters/Player/PlayerController.cs
@@ -18,32 +18,25 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator.GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void FixedUpdate() {
         //If movement input is not 0, try to move
-        if(movementInput != Vector2.zero) {
-          bool success = TryMove(movementInput);
+        if (movementInput != Vector2.zero) {
+            bool success = TryMove(movementInput);
 
-            if(!success) {
+            if (!success) {
                 success = TryMove(new Vector2(movementInput.x, 0));
 
-                if(!success) {
+                if (!success) {
                     success = TryMove(new Vector2(0, movementInput.y));
-                }
+                } 
             }
 
-                animator.SetBool("isMoving", success); 
-            } else {
-                animator.SetBool("isMoving", false);
-            }
+            
         }
+
+    }
 
     private bool TryMove(Vector2 direction) {
         // Check for potnetial collisions
