c04f0e6c0e783a35503d260ca7ab771ef0726408
notpr1me <minimofreeman@gmail.com>
2023-10-03 17:18:47 -0700

    rework enemy

--- Assets/Characters/Enemy/Enemy.cs
+++ Assets/Characters/Enemy/Enemy.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,32 +9,34 @@ public class Enemy : MonoBehaviour {
     public float speed = 3f;
     public float rotateSpeed = 0.0025f;
     private Rigidbody2D rb;
-
      SpriteRenderer spriteRenderer;
 
-    private void Start() {
+    private void Start() { 
+            spriteRenderer = GetComponent<SpriteRenderer>();
             rb = GetComponent<Rigidbody2D>();
             spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update () {
         if (!target) {
-              GetTarget();
-        }   else {
-           RotateTowardsTarget();
-        
-            }
+            GetTarget();
+        } else {
+            RotateTowardsTarget();
         }
+    }
 
     private void FixedUpdate() {
         rb.velocity = transform.up * speed;
     }
 
-    private void RotateTowardsTarget() {
-        Vector2 targetDirection = target.position - transform.position;
-        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
-        Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
-        transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
+    
+
+
+        private void RotateTowardsTarget() {
+            Vector2 targetDirection = target.position - transform.position;
+            float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+            Quaternion q = Quaternion.Euler(new Vector3(0, 0, angle));
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, q, rotateSpeed);
 
          if(targetDirection.x < 0) {
                 spriteRenderer.flipX = true;
@@ -55,7 +58,3 @@ public class Enemy : MonoBehaviour {
         }
     }
 }
-
-
-
-   
