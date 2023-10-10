d948b12408b2d30348d8b164b763a34657e8ac66
notpr1me <minimofreeman@gmail.com>
2023-10-10 08:29:02 -0700

    damage

--- Assets/DamageScript.cs
+++ Assets/DamageScript.cs
@@ -1,16 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DamageScript : MonoBehaviour
 {
-    public float projectileLifetime = 5f;
-    public float projectileSpeed = 10f;
+    public float projectileLifetime = 3f;
+    public float projectileSpeed = 5f;
     public int projectileDamage = 10;
 
     private Rigidbody2D rb;
 
     private bool isDestroyed = false;
+
+    void Start()
+    {
+        // Set the lifetime timer
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(gameObject, projectileLifetime);
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -27,25 +32,18 @@ public class DamageScript : MonoBehaviour
                 enemyHealth.TakeDamage(projectileDamage);
             }
             isDestroyed = true;
-            Destroy(gameObject);
+            Destroy(rb.gameObject);
         }
         else if (other.CompareTag("Tilemap"))
         {
             isDestroyed = true;
-            Destroy(gameObject);
+            Destroy(rb.gameObject);
         }
         else if (other.CompareTag("Collisions"))
         {
             isDestroyed = true;
-            Destroy(gameObject);
-        }
-        else if (other.CompareTag("Player") && other.gameObject != gameObject.transform.parent.gameObject)
-        {
-            // Add code to execute when colliding with other players
-        }
-        else
-        {
-            // Add code to execute when colliding with other objects
+            Destroy(rb.gameObject);
         }
     }
 }
+
